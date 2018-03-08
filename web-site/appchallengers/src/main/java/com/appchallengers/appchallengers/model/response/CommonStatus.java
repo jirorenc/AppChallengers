d82d23de570e9ed745187f72144d84e09906469e
Furkan Önel <onelfurkan@gmail.com>
2018-03-08 12:47:39 +0200

    Login-Logout system and session managment system was created

---
+++ web-site/appchallengers/src/main/java/com/appchallengers/appchallengers/model/response/CommonStatus.java
@@ -0,0 +1,10 @@
+package com.appchallengers.appchallengers.model.response;
+
+public class CommonStatus {
+
+    private Integer statusCode;
+
+    public CommonStatus(Integer statusCode) {
+        this.statusCode = statusCode;
+    }
+}
